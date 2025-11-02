f277d9afa2efadf762dd1e674a8bd192a417a8be
SolveSaint <solvesaint5@gmail.com>
2025-11-01 20:35:31 -0400

    Deploy Wiki: 2025-11-01 08:35:31 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -2,8 +2,8 @@ import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
 // Direct component imports (custom)
-import GoatCounterSPA from "./quartz/components/GoatCounterSPA"
-import TagListToggle from "./quartz/components/TagListToggle"
+//import GoatCounterSPA from "./quartz/components/GoatCounterSPA"
+//import TagListToggle from "./quartz/components/TagListToggle"
 
 // Local sort: newest modified/created first, then A→Z by title
 const byDateAndAlphabetical = (a: any, b: any) => {
