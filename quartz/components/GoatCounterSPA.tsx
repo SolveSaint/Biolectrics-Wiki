43150e6fcad366cd4b51512838162a73790caf67
SolveSaint <solvesaint5@gmail.com>
2025-11-01 20:22:41 -0400

    Deploy Wiki: 2025-11-01 08:22:41 PM

--- quartz/components/GoatCounterSPA.tsx
+++ quartz/components/GoatCounterSPA.tsx
@@ -1,13 +1,13 @@
-import { QuartzComponent, QuartzComponentConstructor } from "./types"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import goatcounterNav from "./goatcounter.inline"
 
-export default (() => {
-  const C: QuartzComponent = {
-    name: "GoatCounterSPA",
-    // Render nothing into the HTML, we only need the hook
-    render: () => null,
-    // Attach SPA analytics after DOM is ready
-    afterDOMLoaded: goatcounterNav,
-  }
-  return C
-}) satisfies QuartzComponentConstructor
+// Must be a function component, not a plain object
+const GoatCounterComponent: QuartzComponent = (_props: QuartzComponentProps) => {
+  // nothing to render; we only hook SPA nav
+  return null
+}
+
+// attach SPA hook
+GoatCounterComponent.afterDOMLoaded = goatcounterNav
+
+export default (() => GoatCounterComponent) satisfies QuartzComponentConstructor
